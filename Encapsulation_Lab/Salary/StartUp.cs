9c52493d4e519b80803966c539c6df4d8d16285c
anidz <96003907+anidz@users.noreply.github.com>
2022-02-26 20:52:17 +0200

    Encapsulation Lab

---
+++ Encapsulation_Lab/Salary/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        
+            public static void Main(string[] args)
+            {
+                Engine engine = new Engine();
+
+                engine.Proceed();
+            }
+        }
+    }
+
