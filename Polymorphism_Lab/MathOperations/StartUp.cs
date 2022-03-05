2ba49b328dfd1a97ce9ed9242f2d3112dce0d968
anidz <96003907+anidz@users.noreply.github.com>
2022-03-05 18:25:20 +0200

    Polymorphism Lab

---
+++ Polymorphism_Lab/MathOperations/StartUp.cs
@@ -0,0 +1,16 @@
+﻿
+using Operations.Core;
+using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Proceed();
+        }
+    }
+}
