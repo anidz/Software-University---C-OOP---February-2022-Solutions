f0b3f3d2b35ee09601467f156700be673d380705
anidz <96003907+anidz@users.noreply.github.com>
2022-03-12 18:09:53 +0200

    Exceptions And Error Handling Lab

---
+++ ExceptionsAndErrorHandling_Lab/01.SquareRoot/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class StartUp
+    {
+        private const string INVALID_MESSAGE = "Invalid number.";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var number = uint.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{Math.Sqrt(number)}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(INVALID_MESSAGE);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
+
