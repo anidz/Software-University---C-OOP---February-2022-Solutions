fe87b1121b7bd05526708d63ec2f943c1f19429b
anidz <96003907+anidz@users.noreply.github.com>
2022-03-19 20:30:11 +0200

    Reflection And Attributes Ex

---
+++ ReflectionAndAttributes_Exercise/ValidationAttributes/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ValidationAttributes.Core;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //var person = new Person
+            // (
+            //     null,
+            //     -1
+            // );
+
+            //bool isValidEntity = Validator.IsValid(person);
+
+            //Console.WriteLine(isValidEntity);
+
+           
+            {
+                Engine engine = new Engine();
+
+                engine.Proceed();
+            }
+        }
+    }
+}
